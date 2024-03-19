de355a6d891e1be22f1d74f004f538fc205d41db
Wirayudhas <arturwirayuda@gmail.com>
2024-03-19 10:41:47 +0700

    Menambahkan perulangan "for" kedua

--- 196_LoopingFungsi/196_LoopingFungsi.cpp
+++ 196_LoopingFungsi/196_LoopingFungsi.cpp
@@ -10,6 +10,13 @@ int main() {
 		cout << i << "selamat pagi dunia" << endl;
 	}
 
+	cout << "niai i terakhir : " << i << endl;
+
+	for (i = 0; i < 5; i++) {
+		cout << "masukan nilai index ke-" << i << " :";
+		cin >> arr[i];
+	}
+
 	
 	
 
